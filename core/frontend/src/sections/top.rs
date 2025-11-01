77709d2ad650b8e47636c88770f30f4ce4f3c361
stifskere <esteve@memw.es>
2025-11-01 09:56:09 +0100

    chore(coming-soon-page): Setup Translations.

--- core/frontend/src/sections/top.rs
+++ core/frontend/src/sections/top.rs
@@ -1,33 +1,92 @@
+use translatable::translation;
 use yew::prelude::*;
+
+use crate::utils::language::inferred_browser_language;
 
 #[function_component(TopSection)]
 pub fn top_section() -> Html {
+    let browser_language = use_state(inferred_browser_language);
+
+    // TODO: Implement smiling face.
+    // TODO: Translate CV in three langauges.
+    // TODO: Add rustc version footer.
+
     html! {
         <section class="top-section-container">
             <h1>{"Memw"}</h1>
             <p>
-                {"This page is coming soon, "}
+                {
+                    translation!(
+                        (*browser_language).clone(),
+                        static sections::top::subtitle::first_p
+                    )
+                        .expect("Translation to exist.")
+                }
                 <a
                     class="text-link"
                     href="/assets/provisional-cv.pdf"
                 >
-                    {"you can download my CV"}
+                    {
+                        translation!(
+                            (*browser_language).clone(),
+                            static sections::top::subtitle::cv_link
+                        )
+                            .expect("Translation to exist.")
+                    }
                 </a>
-                {" anyways."}
+                {
+                    translation!(
+                        (*browser_language).clone(),
+                        static sections::top::subtitle::last_p
+                    )
+                        .expect("Translation to exist.")
+                }
             </p>
 
             <aside class="bottom-left-aside">
-                <h2>{format!("This instance was built in {}", env!("BUILD_TIME"))}</h2>
+                <h2>
+                    {
+                        translation!(
+                            (*browser_language).clone(),
+                            static sections::top::instance_build,
+                            build_time = env!("BUILD_TIME")
+                        )
+                            .expect("Translation to exist.")
+                    }
+                </h2>
+                <p>{env!("RUSTC_VERSION")}</p>
             </aside>
 
             <aside class="bottom-right-aside">
-                <h2>{"Expect Great Things ;D"}</h2>
-                <p>{"Stay tunned about the "}
+                <h2>
+                    {
+                        translation!(
+                            (*browser_language).clone(),
+                            static sections::top::motivational_quote,
+                            smiling_face = ";D"
+                        )
+                            .expect("Translation to exist.")
+                    }
+                </h2>
+                <p>
+                    {
+                        translation!(
+                            (*browser_language).clone(),
+                            static sections::top::portfolio_development::first_p
+                        )
+                            .expect("Translation to exist.")
+                    }
                     <a
                         class="text-link"
                         href="https://github.com/stifskere/portfolio"
                     >
-                        {"portfolio development."}
+                        {
+                            translation!(
+                                (*browser_language).clone(),
+                                static sections::top::portfolio_development::link
+                            )
+                                .expect("Translation to exist.")
+                        }
                     </a>
                 </p>
             </aside>
